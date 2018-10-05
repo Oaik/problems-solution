43888321b39248502dd14095f98246b94567b020
Omar Ashraf <omarashrf15@gmail.com>
2018-10-05 20:34:00 +0200

    added some (UvA and DIV-B) problems

--- codeforces/div-B/src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const GET_ITEMS = 'GET_ITEMS';
-export const ADD_ITEM = 'ADD_ITEM';
-export const DELETE_ITEM = 'DELETE_ITEM';
-export const ITEMS_LOADING = 'ITEMS_LOADING';
--- codeforces/div-B/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import itemReducer from './itemReducer';
-
-export default combineReducers({
-  item: itemReducer
-});
