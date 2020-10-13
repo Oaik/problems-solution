32c8fe04462ae0913f171963af9344b4edeb8e40
Omar Ashraf <omarashrf15@gmail.com>
2020-10-13 07:14:14 +0200

    Update PTDS.cpp

--- Libary for PS/PTDS.cpp
+++ Libary for PS/PTDS.cpp
@@ -4,4 +4,4 @@
 using namespace __gnu_pbds;
  
 template<typename T>
-using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using indexedSet = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
