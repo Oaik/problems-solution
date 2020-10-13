a0b29f433d820dbec8489bf8fea2e132aa711029
Omar Ashraf <omarashrf15@gmail.com>
2020-10-13 07:00:36 +0200

    Update PTDS.cpp

--- Libary for PS/PTDS.cpp
+++ Libary for PS/PTDS.cpp
@@ -1,12 +1,7 @@
 #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/trie_policy.hpp>
-
+#include <ext/pb_ds/tree_policy.hpp>
+ 
 using namespace __gnu_pbds;
-
-typedef tree<
-int,
-null_type,
-less<int>,
-rb_tree_tag,
-tree_order_statistics_node_update>
-ordered_set;
+ 
+template<typename T>
+using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
