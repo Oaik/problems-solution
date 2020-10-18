06fbff9fea69cb36ecbad3e7769db296840f32ee
Omar Ashraf <omarashrf15@gmail.com>
2020-10-19 01:06:44 +0200

    Update tarjan.cpp

--- Libary for PS/tarjan.cpp
+++ Libary for PS/tarjan.cpp
@@ -1,26 +1,33 @@
-void tarjan(int u){
-    dfs_num[u] = dfs_low[u] = tme++;
-    vis[u] = true;
-    best[u] = u;
-    st.push(u);
-    inStack[u] = true;
-    for(int c : adj[u]){
-        if(!vis[c]) {
-            tarjan(c);
-            dfs_low[u] = min(dfs_low[u] , dfs_low[c]);
-        }
-        if(inStack[c])dfs_low[u] = min(dfs_low[u] , dfs_num[c]);
+int lowlink[N], dfn[N];
+bool instack[N];
+int num = 0;
+vector<int> g[N];
+stack<int> stack_nodes;
+int ans = 0;
+ 
+void tarjan(int node) {
+    lowlink[node] = dfn[node] = ++num;
+    instack[node] = 1;
+    stack_nodes.push(node);
+    for (int i = 0; i < g[node].size(); i++) {
+        int v = g[node][i];
+        if (!dfn[v]) {
+            tarjan(v);
+            lowlink[node] = min(lowlink[node], lowlink[v]);
+        } else if (instack[v])
+            lowlink[node] = min(lowlink[node], dfn[v]);
     }
-    if(dfs_low[u] == dfs_num[u]) {
-        while(1){
-            int v = st.top();st.pop();
-            compRoot[v] = u;
-            inStack[v] = false;
-            if(u == v)break;
+    if (lowlink[node] == dfn[node]) {
+        int v = -1;
+        vector<int> cyc;
+        while (v != node) {
+            v = stack_nodes.top();
+            stack_nodes.pop(), instack[v] = 0;
+            cyc.push_back(v);
         }
+        if (cyc.size() > 1) ans += cyc.size();
     }
 }
-
 
 for(int i = 0 ; i < idx ; ++i){
     if(!vis[i])
